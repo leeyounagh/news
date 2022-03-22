a0c2b23f8397d95398bd618661ebbafa64a83d1e
youngah lee <dudgk0216@naver.com>
2022-03-23 02:49:32 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header'
+import Navigation from './Components/Navigation'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component{
+ 
+  render(){
+    return (
+     
+      <div className="App container">
+       <Header></Header>
+      <Navigation></Navigation>
+      
+        <div className="row news">
+        <div className="col-lg-8"> 
+        <img   alt="news"className="news-img-size No-Image" 
+        src="img/hyun.jpg"></img>
+                
+        </div>
+            <div className="col-lg-4">
+               <h2>스트레이키즈 컴백</h2>
+               <p> 스트레이키즈가 오늘 컴백했다고 한다 </p>            
+              <div>
+                2022-03-01
+              </div>
+            </div>
+        </div>
+        
     </div>
-  );
+     
+    );
+  }
+
+
+  
 }
 
-export default App;
+
---
+++ src/Components/Navigation.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class Navigation extends Component {
+  render() {
+    return (
+        <div className="menus">
+        <button>sport</button>
+        <button>news</button>
+        <button>tech</button>
+        <button>world</button>
+        <button>finance</button>
+        <button>politics</button>
+        <button>business</button>
+        <button>economics</button>
+        <button>entertainment</button>
+        <button>beauty</button>
+        <button>travel</button>
+        <button>music</button>
+        <button>food</button>
+        <button>science</button>
+        <button>gaming</button>
+        <button>energy</button>
+      
+        </div>
+    )
+  }
+}
