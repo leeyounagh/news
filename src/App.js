d9435bcbfd4028abb2ddb9f4cc40fdd842a526a9
leeyoungah <dudgk0216@naver.com>
2022-03-27 01:04:24 +0900

    sixth commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './Components/Header'
 import Navigation from './Components/Navigation'
 import Main from './Components/Main';
 
+
+
 import { v4 as uuidv4 } from 'uuid';
 
 
@@ -14,6 +16,7 @@ export default class App extends Component{
   
   constructor(props){
     let url;
+    let page =1;
     super(props);
     this.state ={
       latestnews:[],
@@ -24,38 +27,58 @@ export default class App extends Component{
 
   componentDidMount() {
     this.getLatestNews();
+    this.getNews();
+    
+    
   } //fech함수가 render 함수보다 늦게 호출되기때문에 componentDidMount()함수에 
   // 해당함수를 넣어야됨
  
     
    getNews = async ()=>{
-    let news = [];
-        //  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=kR&topic=business&page_size=100`)  
-        //  console.log("url",url)
-          let header = new Headers({'x-api-key':'L7DBi0iMqKfM98Mdh44oqyBRYYdkbQPgaiTE93Pj3-U'})
-         let response =await fetch(this.url,{headers:header})
-          let data = await response.json();
-          news = data.articles
-          JSON.stringify(news) //제이슨 파일을 스트링화해주는거 
-          this.setState(
-            {
-              latestnews:news
-            }
-          )
-          console.log("data",this.state.latestnews)
-          return news
+    
+      let news = [];
+       
+       
+           
+        
+       
+      let header = new Headers({'x-api-key':'L7DBi0iMqKfM98Mdh44oqyBRYYdkbQPgaiTE93Pj3-U'})
+
+     let response =await fetch(this.url,{headers:header})
+      let data = await response.json();
+      news = data.articles
+       this.page = data.page
+      
+        JSON.stringify(news) //제이슨 파일을 스트링화해주는거 
+      this.setState(
+        {
+          latestnews:news
+        }
+      )
+      console.log("data",this.state.latestnews)
+      console.log("새로운 알엘이",this.url)
+      
+      return news
      
-          }
+     
+      
+     }
+   
+        
 
       getLatestNews= async ()=>{
-          this.url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=kR&topic=business&page_size=100`)  
+          this.url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=kR&topic=business&page_size=10`)  
+        //   let params = new URLSearchParams(this.url.search);
+        // //  params.append('page',this.page)//새로운 파라미터 추가
+        // params.set('page', 1)
+        // params.toString();
+
           console.log("url",this.url)
           this.getNews()
           
       } 
 
-
-   
+  
     
  
       
@@ -67,7 +90,7 @@ export default class App extends Component{
     return <div key={uuidv4()} className="row news news-style" id='news-board'>
     <div key={uuidv4()} className="col-lg-8"> 
     <img  key={uuidv4()}  alt="news"className="news-img-size No-Image" 
-   src={news.media}></img>
+   src={news.media ||  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"}></img>
           
        </div>
       <div key={uuidv4()} className="col-lg-4">
@@ -88,24 +111,31 @@ export default class App extends Component{
    })
    return _news
    }
-
+  
      
   
 // //각각의 값에 키값만 부여하면되는데..
    
 
   render(){
-    console.log("나와이새꺄", this.state.latestnews)
+  
     
     return (
     
       <div className="App container">
        
        <Header></Header>
-       <Navigation></Navigation>
        
+       <Navigation getTopic={function async(e){
+        this.url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=kR&topic=${e.target.textContent}&page_size=10`)
+        this.getNews()
+        
+        console.log("토픽",this.url)
+         
+       }.bind(this)}></Navigation>
       
        { this._renderNews()}
+
       
       
     
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,29 +1,76 @@
 import React, { Component } from 'react'
 
 export default class Navigation extends Component {
-  // getTopic= async(e)=>{
-  //   let topic = {e.target.textContext}
-  //   this.url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=kR&topic={${topic}}&page_size=100`)
-  // }
+ 
+
+
   render() {
     return (
         <div className="menus">
-        <button>sport</button>
-        <button>news</button>
-        <button>tech</button>
-        <button>world</button>
-        <button>finance</button>
-        <button>politics</button>
-        <button>business</button>
-        <button>economics</button>
-        <button>entertainment</button>
-        <button>beauty</button>
-        <button>travel</button>
-        <button>music</button>
-        <button>food</button>
-        <button>science</button>
-        <button>gaming</button>
-        <button>energy</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>sport</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>news</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>tech</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>world</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>finance</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>politics</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>business</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>economics</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>entertainment</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>beauty</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>travel</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>music</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}> food</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>science</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>gaming</button>
+        <button onClick={function(e){
+          e.preventDefault();
+          this.props.getTopic(e)
+        }.bind(this)}>energy</button>
       
         </div>
     )
